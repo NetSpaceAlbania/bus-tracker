cea71c351f6634caffc03bcfff17d6b7f442e52b
hackerspacealbania <uljan89@gmail.com>
2021-05-07 16:02:29 +0100

    added map marker

--- live_map/static/leaf.js
+++ live_map/static/leaf.js
@@ -1,5 +1,5 @@
 
-var mymap = L.map('mapid').setView([41.3268505,19.8205273], 13);
+var albmap = L.map('mapid').setView([41.3268505,19.8205273], 13);
  
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
@@ -8,5 +8,7 @@ maxZoom: 18,
 zoomOffset: -1,
 id: 'mapbox/streets-v11',
 accessToken: 'pk.eyJ1IjoiaGFja2Vyc3BhY2VhbGJhbmlhIiwiYSI6ImNrb2E3dHczazAzeDIycG9kY2EyMjV1dncifQ.LnFZVoR69jnFIIZImLZOmw'
-}).addTo(mymap);
+}).addTo(albmap);
 
+// adding markers for the bus stations
+var marker = L.marker([41.325, 19.820]).addTo(albmap);
