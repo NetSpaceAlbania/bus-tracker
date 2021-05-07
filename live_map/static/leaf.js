462b7b2d2b9f670efb8dbbda786d6bd09617b2a4
hackerspacealbania <uljan89@gmail.com>
2021-05-07 12:38:21 +0100

    adding live maps

---
+++ live_map/static/leaf.js
@@ -0,0 +1,11 @@
+
+var mymap = L.map('mapid').setView([41.3268505,19.8205273], 13);
+ 
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+tileSize: 512,
+maxZoom: 18,
+zoomOffset: -1,
+id: 'mapbox/streets-v11',
+accessToken: 'pk.eyJ1IjoiaGFja2Vyc3BhY2VhbGJhbmlhIiwiYSI6ImNrb2E3dHczazAzeDIycG9kY2EyMjV1dncifQ.LnFZVoR69jnFIIZImLZOmw'
+}).addTo(mymap);
