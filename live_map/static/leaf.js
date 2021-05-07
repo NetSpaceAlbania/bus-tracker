f5105723be630234ccdf198969575c8c7e48af7f
hackerspacealbania <uljan89@gmail.com>
2021-05-07 20:32:09 +0100

    flask app and kafka

--- live_map/static/leaf.js
+++ live_map/static/leaf.js
@@ -1,6 +1,6 @@
+// tile layer for the map
 
 var map = L.map('map').setView([41.3268505,19.8205273], 13);
- 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
 tileSize: 512,
@@ -9,6 +9,7 @@ zoomOffset: -1,
 id: 'mapbox/streets-v11',
 accessToken: 'pk.eyJ1IjoiaGFja2Vyc3BhY2VhbGJhbmlhIiwiYSI6ImNrb2E3dHczazAzeDIycG9kY2EyMjV1dncifQ.LnFZVoR69jnFIIZImLZOmw'
 }).addTo(map);
+
 
 // adding markers for the bus stations
 
@@ -33,18 +34,32 @@ var busStop = L.icon({
 L.marker([41.350581, 19.831421], {icon: busStop}).addTo(map);
 
 
-var stationMarker = L.marker([41.306576, 19.796567]).addTo(map);
+var busline = L.marker([41.306576, 19.796567]).addTo(map);
 stationMarker.bindPopup("Starting station").openPopup();
 
-var stationMarker2 = L.marker([41.350581, 19.831421]).addTo(map);
-stationMarker2.bindPopup("End station").openPopup(False);
-
-// bus line layer, fontawesome
-
-//var layer = L.marker([41.3, 19.8]).addTo(map);
-
-//L.control.layers(null, {'<i class="fas fa-bus"></i>': layer}).addTo(map);
-
-// bus icon
+//var busline2 = L.marker([41.35, 19.83]).addTo(map);
+//busline2.bindPopup("End station").openPopup(False);
 
 
+// Simulation of live buses
+
+linja1 = [];
+
+
+var source = new EventSource('/topic/TOPICNAME'); //ENTER YOUR TOPICNAME HERE
+source.addEventListener('message', function(e){
+
+  console.log('Message');
+  obj = JSON.parse(e.data);
+  console.log(obj);
+
+  if(obj.busline == '00001') {
+    for (var i = 0; i < mapMarkers1.length; i++) {
+      mymap.removeLayer(mapMarkers1[i]);
+    }
+    marker1 = L.marker([obj.latitude, obj.longitude]).addTo(mymap);
+    mapMarkers1.push(marker1);
+  }
+
+
+}, false);
