dfbd6da1b8efe0aea99eb3889b3c20be911a8f66
hackerspacealbania <uljan89@gmail.com>
2021-05-07 17:00:34 +0100

    adding markers and popups

--- live_map/static/leaf.js
+++ live_map/static/leaf.js
@@ -11,4 +11,31 @@ accessToken: 'pk.eyJ1IjoiaGFja2Vyc3BhY2VhbGJhbmlhIiwiYSI6ImNrb2E3dHczazAzeDIycG9
 }).addTo(albmap);
 
 // adding markers for the bus stations
-var marker = L.marker([41.325, 19.820]).addTo(albmap);
+var stationMarker = L.marker([41.306576, 19.796567]).addTo(albmap);
+stationMarker.bindPopup("First station marker").openPopup();
+
+var stationMarker2 = L.marker([41.350581, 19.831421]).addTo(albmap);
+stationMarker2.bindPopup("Last station marker").openPopup();
+
+// adding a standalone popup
+
+var popup = L.popup()
+    .setLatLng([41.33, 19.83])
+    .setContent("Selitë-Kristal-Qendër-Stacioni i Trenit-Allias")
+    .openOn(albmap);
+
+    // Map Events
+
+    var popup = L.popup();
+
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("Koodrinata e zgjedhur eshte: " + e.latlng.toString())
+            .openOn(albmap);
+    }
+    
+    albmap.on('click', onMapClick);
+
+
+// representing a GeoJSON
