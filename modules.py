bdb80777fc36b35f361cb7b5744a580af03daf96
hackerspacealbania <uljan89@gmail.com>
2021-05-14 22:20:04 +0100

    adding models, modules, network

---
+++ modules.py
@@ -0,0 +1,12 @@
+"""modules... TTGO-T-Beam, ESP32, lora, gps"""
+def gps():
+    """encode captured gps data to byte"""
+    pass
+
+def getlatlon():
+    """get latitude and longitude from GPS"""
+    pass
+
+def correction():
+    "error correction and calibration of modules"
+
