09018f79b94320147873edccc4ee2170a7e0cb04
hackerspacealbania <uljan89@gmail.com>
2021-05-20 13:27:48 +0100

    adding circle leafletjs

--- live_map/static/leaf.js
+++ live_map/static/leaf.js
@@ -15,52 +15,17 @@ var marker1 = L.marker([41.3268305,19.8205273]).addTo(map);
 // var marker2 = L.marker([lon,lat]).addTo(map);
 // var marker3 = L.marker([41.3368305,19.8305273]).addTo(map);
 
+// LoRA network coverage
+
+var circle = L.circle([41.3268305,19.8205273], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 3
+}).addTo(map);
+
+
+// Geojson feature
+
+
 // Simulation of live buses
-
-mapMarkers1 = [];
-
-
-var myObj, myJSON, text, obj;
-
-// Storing data:
-myObj = { name: "John", age: 31, city: "New York" };
-myJSON = JSON.stringify(myObj);
-localStorage.setItem("testJSON", myJSON);
-
-// Retrieving data:
-text = localStorage.getItem("testJSON");
-obj = JSON.parse(text);
-document.getElementById("demo").innerHTML = obj.name;
-
-myFunction(myArray);
-
-function myFunction(arr) {
-  var out = "";
-  var i;
-  for(i = 0; i < arr.length; i++) {
-    out += '<a href="' + arr[i].url + '">' + 
-    arr[i].display + '</a><br>';
-  }
-  document.getElementById("id01").innerHTML = out;
-}
-
-
-//var source = new EventSource('/topic/TOPICNAME'); //ENTER YOUR TOPICNAME HERE
-var source = {"lon:":41.368305, "lat":19.8205273}
-
-source.addEventListener('message', function(e){
-
-  console.log('Message');
-  obj = JSON.parse(e.data);
-  console.log(obj);
-
-  if(obj.busline == '00001') {
-    for (var i = 0; i < mapMarkers1.length; i++) {
-      map.removeLayer(mapMarkers1[i]);
-    }
-    marker1 = L.marker([obj.latitude, obj.longitude]).addTo(map);
-    mapMarkers1.push(marker1);
-  }
-
-
-}, false);
--- network.py
+++ network.py
@@ -1,61 +1,24 @@
 """
 Simulimi i rrjetit te komunikimit midis pajisjeve
 
-Potential issues:
-    network management
-    collection of coordinates
-    implementation of gateways
-    microcontrollers
-    decentralized network
-    Global Positioning System
-    LoRA range
-    Stations far apart
-    Tracking the moment when bus reaches at least one station
-    best aalternatives to buld the network? GPS, LoRA
-    Energy consumption bluetooth, wifi. Source solar panels
-    proper device setup
-    edge cases/scenarios i.e. buses one after another at opposite sides
-    model scenarios
-    communication frequencies, bus stop, bus module freq tuner
-    ETA, time calculations behind the model
-    testing existing solutions, maps APIs and mobile phone GPS
-    proper plan and team for the project
-    what is the simplest solution to start off with
-
-
 """
 
 import time
 
 # Komunikimi midis tre nyjeve
-i = 0
-while i <= 11:
-    node1 = "hello"
-    i += 1
 
-    print(node1)
+def nyja1():
+    tx= True
+    rx = False
+    nid='nyja1'
+    packet=[123]
 
-node2 = 2
+    print(packet)
 
-node3 = 3
 
-def probability():
-    # Random variable representing number of buses
-    # Random variable representing number of buses
-# Mean number of buses coming to bus stop in 30 minutes is 1
-#
-X = [0, 1, 2, 3, 4]
-lmbda = 1
-#
-# Probability values
-#
-poisson_pd = poisson.pmf(X, lmbda)
-#
-# Plot the probability distribution
-#
-fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-ax.plot(X, poisson_pd, 'bo', ms=8, label='poisson pmf')
-plt.ylabel("Probability", fontsize="18")
-plt.xlabel("X - No. of Buses", fontsize="18")
-plt.title("Poisson Distribution - No. of Buses Vs Probability", fontsize="18")
-ax.vlines(X, 0, poisson_pd, colors='b', lw=5, alpha=0.5)
+
+
+if __name__ == '__main__':
+    nyja1()
+    #nyja2()
+    #nyja3()
